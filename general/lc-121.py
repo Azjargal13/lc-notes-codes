2c0989fb0078a5687d44066d150cba447731e5ac
Azjargal13 <azjargal.gankhuyag@konicaminolta.com>
2020-04-12 15:21:13 +0900

    max profit

---
+++ general/lc-121.py
@@ -0,0 +1,18 @@
+# 121. Best Time to Buy and Sell Stock
+
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        mcur = 0
+        mSoFar = 0
+        # kadane's algo
+        for i in range(1, len(prices)):
+            mcur += prices[i] - prices[i-1]
+            mcur = max(0, mcur)
+            mSoFar = max(mcur, mSoFar)
+        return mSoFar
+
+
+s = Solution()
+p = [7, 1, 5, 3, 6, 4]
+print(s.maxProfit(p))
